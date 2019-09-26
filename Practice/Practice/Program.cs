8b8086f6567977302a5cd0739fa35990ed87387c
veeskela\Admin <paulalexeev10@gmail.com>
2019-09-26 20:37:29 +0300

    *added first practice

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Practice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const string MorningText = "Good morning, guys";
+            const string DayText = "Good day, guys";
+            const string EveningText = "Good evening, guys";
+            const string NightText = "Good night, guys";
+
+            var today = DateTime.Now.TimeOfDay;
+
+            if (today.Hours > 9 && today.Hours < 12)
+            {
+                Console.WriteLine(MorningText);
+            }
+            else if (today.Hours > 12 && today.Hours < 15)
+            {
+                Console.WriteLine(DayText);
+            }
+            else if (today.Hours > 15 && today.Hours < 22)
+            {
+                Console.WriteLine(EveningText);
+            }
+            else if (today.Hours > 22 && today.Hours < 9)
+            {
+                Console.WriteLine(NightText);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
