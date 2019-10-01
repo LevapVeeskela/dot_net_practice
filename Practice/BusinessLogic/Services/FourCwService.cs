b8b99fab0885888ac99ea829be2ce2024459e590
veeskela\Admin <paulalexeev10@gmail.com>
2019-10-01 20:28:38 +0300

    *take out logic of method in helper for four practice

--- Practice/BusinessLogic/Services/FourCwService.cs
+++ Practice/BusinessLogic/Services/FourCwService.cs
@@ -1,5 +1,6 @@
 ﻿using Common.Constants;
 using System;
+using Common.Helpers;
 
 namespace BusinessLogic.Services
 {
@@ -8,25 +9,9 @@ namespace BusinessLogic.Services
         public void GetCurrentTime()
         {
             var today = DateTime.Now.TimeOfDay;
-
-            if (today.Hours >= 9 && today.Hours < 12)
-            {
-                Console.WriteLine(FourCw.MorningText);
-            }
-            else if (today.Hours >= 12 && today.Hours < 15)
-            {
-                Console.WriteLine(FourCw.DayText);
-            }
-            else if (today.Hours >= 15 && today.Hours < 22)
-            {
-                Console.WriteLine(FourCw.EveningText);
-            }
-            else if (today.Hours >= 22 && today.Hours < 9)
-            {
-                Console.WriteLine(FourCw.NightText);
-            }
-
             var stringTime = $"Right now time: {today.Hours}:{today.Minutes}:{today.Seconds}";
+
+            Console.WriteLine(FourCwHelper.GetCurrentTimeInString(today));
             Console.WriteLine(stringTime);
         }
     }
---
+++ Practice/Common/Helpers/FourCwHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Common.Constants;
+
+namespace Common.Helpers
+{
+    public static class FourCwHelper
+    {
+        public static string GetCurrentTimeInString(TimeSpan day) => day switch
+        {
+            _ when day.Hours >= 9 && day.Hours < 12 => FourCw.MorningText,
+            _ when day.Hours >= 12 && day.Hours < 15 => FourCw.DayText,
+            _ when day.Hours >= 15 && day.Hours < 22 => FourCw.EveningText,
+            _ when day.Hours >= 22 && day.Hours < 9 => FourCw.NightText
+        };
+    }
+}
