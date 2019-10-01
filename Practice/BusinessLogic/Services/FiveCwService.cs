fbb426239ea78fc3aa7c42f95c2e4f3ead6d53c8
veeskela\Admin <paulalexeev10@gmail.com>
2019-10-01 21:29:54 +0300

    *finished task for five lesson

---
+++ Practice/BusinessLogic/Services/FiveCwService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BusinessLogic.Services
+{
+    public class FiveCwService
+    {
+        public void WorkWithArray()
+        {
+            var tempArrat = new int[] {
+                new Random().Next(0, 50),
+                new Random().Next(0, 50),
+                new Random().Next(0, 50),
+                new Random().Next(0, 50),
+                new Random().Next(0, 50),
+                new Random().Next(0, 50)
+            };
+            WriteInConsole(tempArrat);
+            Console.WriteLine(new string('-', 30));
+            tempArrat[2] = tempArrat[2] * 10;
+            WriteInConsole(tempArrat);
+        }
+
+        public void WriteInConsole(int[] intArray)
+        {
+            foreach (var item in intArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
