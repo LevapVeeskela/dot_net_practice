e829a28aedf805f1a8d91ad4b8756e8cffe4d86f
veeskela\Admin <paulalexeev10@gmail.com>
2019-10-03 19:58:05 +0300

    *finished four tasks

---
+++ Practice/BusinessLogic/Services/SixCwService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BusinessLogic.Services
+{
+    public class SixCwService
+    {
+        public void FirstTask()
+        {
+            Console.WriteLine("First Task");
+            const string textPossibleInfo = "Everything is possible in this life";
+            var listInt = new List<int>();
+            int[] emptyArray = listInt.ToArray();
+            WriteInConsole(emptyArray);
+            Console.WriteLine($"{textPossibleInfo}");
+            Console.WriteLine(new string('-', 50));
+        }
+
+        public void SecondTask()
+        {
+            Console.WriteLine("Second Task");
+            object[] arrayThreeItems = { 32, 'A', "Hello" };
+            WriteInConsole(arrayThreeItems);
+            Console.WriteLine(new string('-', 50));
+            arrayThreeItems[0] = (int)arrayThreeItems[0] + 10;
+            arrayThreeItems[1] = $"{arrayThreeItems[1]}, guys!";
+            WriteInConsole(arrayThreeItems);
+            Console.WriteLine(new string('-', 50));
+        }
+
+        public void ThirdTask()
+        {
+            Console.WriteLine("Third Task");
+            var minValue = 0;
+            var maxValue = 50;
+            var randNum = new Random();
+            var tempArray = Enumerable.Repeat(0, 12)
+                .Select(i => randNum.Next(minValue, maxValue))
+                .ToArray();
+            WriteInConsole(tempArray);
+            Console.WriteLine($"Max elem: {tempArray.Max()}");
+            Console.WriteLine(new string('-', 50));
+        }
+
+        public void FourthTask()
+        {
+            Console.WriteLine("Fourth Task");
+            short itemOne = 2;
+            short itemTwo = 3;
+            object[] tempArray = {
+                itemOne, itemTwo
+            };
+            Console.WriteLine("Unboxing при считывание по элементам");
+            WriteInConsole(tempArray);
+        }
+
+        private void WriteInConsole<T>(T[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
