d7868c09821ddaaa2f910a469f1141b4689506de
veeskela\Admin <paulalexeev10@gmail.com>
2019-10-24 20:36:01 +0300

    *added two tasks

---
+++ Practice/Common/Extantions/StringExtanttions.cs
@@ -0,0 +1,11 @@
+﻿namespace Common.Extantions
+{
+    public static class StringExtanttions
+    {
+        public static string SubstringFirstLetter(this string row)
+        {
+            var substring = row.Substring(0, 4);
+            return string.Format(row, "...");
+        }
+    }
+}
---
+++ Practice/ThirteenLessonFirstTask/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BusinessLogic.Services;
+using Common.Extantions;
+using Common.Models;
+
+namespace ThirteenLessonFirstTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunFirstTask();
+            RunSecondTask();
+        }
+
+        static void RunFirstTask()
+        {
+            var song = new SongModel{ NameSong = "NAME", DurationSong = 12, YearSong = 2018, AuthorSong = "I" };
+            var tcfts = new ThirteenCwFirstTaskService(song);
+            var getSong = tcfts.GetSongDataTuple();
+            Console.WriteLine($"{getSong.Item1}, {getSong.Item2}, {getSong.Item3}, {getSong.Item4}");
+        }
+
+        static void RunSecondTask()
+        {
+            Console.Write(Console.ReadLine().SubstringFirstLetter());
+            Console.ReadKey();
+        }
+    }
+}
